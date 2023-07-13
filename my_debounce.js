abb98d62d977212fd68716143a531f7dd70a20e4
stefanlazarevic <stefanlaza97@gmail.com>
2023-07-12 18:07:54 -0700

    commit

--- my_debounce.js
+++ my_debounce.js
@@ -9,39 +9,39 @@ Function.prototype.myDebounce = function(interval) {
     }
 }
 
-class SearchBar {
-    constructor() {
-      this.query = "";
+// class SearchBar {
+//     constructor() {
+//       this.query = "";
   
-      this.type = this.type.bind(this);
-      this.search = this.search.bind(this);
-    }
+//       this.type = this.type.bind(this);
+//       this.search = this.search.bind(this);
+//     }
   
-    type(letter) {
-      this.query += letter;
-      this.search();
-    }
+//     type(letter) {
+//       this.query += letter;
+//       this.search();
+//     }
   
-    search() {
-      console.log(`searching for ${this.query}`);
-    }
-  }
+//     search() {
+//       console.log(`searching for ${this.query}`);
+//     }
+//   }
 
-  const searchBar = new SearchBar();
-  searchBar.search = searchBar.search.myDebounce(500);
+//   const searchBar = new SearchBar();
+//   searchBar.search = searchBar.search.myDebounce(500);
 
-const queryForHelloWorld = () => {
-  searchBar.type("h");
-  searchBar.type("e");
-  searchBar.type("l");
-  searchBar.type("l");
-  searchBar.type("o");
-  searchBar.type(" ");
-  searchBar.type("w");
-  searchBar.type("o");
-  searchBar.type("r");
-  searchBar.type("l");
-  searchBar.type("d");
-};
+// const queryForHelloWorld = () => {
+//   searchBar.type("h");
+//   searchBar.type("e");
+//   searchBar.type("l");
+//   searchBar.type("l");
+//   searchBar.type("o");
+//   searchBar.type(" ");
+//   searchBar.type("w");
+//   searchBar.type("o");
+//   searchBar.type("r");
+//   searchBar.type("l");
+//   searchBar.type("d");
+// };
 
-queryForHelloWorld();
+// queryForHelloWorld();
